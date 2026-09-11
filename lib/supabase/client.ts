4a51daf649565c1361b2f24768e851e65b2727d4
silas39305 <balardjo70@gmail.com>
2026-09-11 17:54:15 +0200

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
+// On initialise et on exporte l'instance "supabase" prête à l'emploi
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
