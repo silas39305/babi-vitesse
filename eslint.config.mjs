c52bbbee023d8f045e3f6b9950c51172ef04557e
silas39305 <balardjo70@gmail.com>
2026-09-11 19:29:12 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // AJOUTER CE BLOC CI-DESSOUS POUR LES APOSTROPHES
+  {
+    rules: {
+      "react/no-unescaped-entities": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
